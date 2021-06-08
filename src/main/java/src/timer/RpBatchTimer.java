7f6a8a31d99c8f71cdf8b0fe8a8f69fb4754353a
Fab <fabo_t@hotmail.com>
2021-06-08 21:04:37 +0200

    timerBatchRp

---
+++ src/main/java/src/timer/RpBatchTimer.java
@@ -0,0 +1,63 @@
+package src.timer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ejb.Timeout;
+import javax.ejb.Timer;
+import javax.ejb.TimerService;
+import javax.inject.Inject;
+
+import src.backingbean.RS3ProductRecovererBatchJobStarterBB;
+
+@Startup
+@Singleton
+public class RpBatchTimer {
+	
+	@Inject
+	private RS3ProductRecovererBatchJobStarterBB batchStarter;
+	
+	
+	
+	@Resource 
+	private TimerService timerService;
+	
+	
+	@PostConstruct
+	public void initTimer() {
+		timerService.createTimer(60000, 60000, null);
+	}
+	
+	
+	@Timeout
+	public void onTime(Timer timer) {
+		recuperaProducto();
+	}
+
+	private void recuperaProducto() {
+		System.out.println("RpBatchTimer.recuperProducto() - ejecutado a las " + new java.util.Date());
+		getBatchStarter().startTask();
+	}
+	
+
+
+	public RS3ProductRecovererBatchJobStarterBB getBatchStarter() {
+		return batchStarter;
+	}
+
+
+	public void setBatchStarter(RS3ProductRecovererBatchJobStarterBB batchStarter) {
+		this.batchStarter = batchStarter;
+	}
+
+
+	public TimerService getTimerService() {
+		return timerService;
+	}
+
+
+	public void setTimerService(TimerService timerService) {
+		this.timerService = timerService;
+	}
+}
